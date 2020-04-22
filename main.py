483e90efe3c0208cfeddfad27fb6c1f60b0bfda8
Shigeyoshi Sakuma <wasaemon0310@gmail.com>
2020-04-22 16:57:30 +0900

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+# -*- coding: SHIFT-JIS -*-
+from flask import Flask, render_template, request
+import numpy as np
+import cv2
+from werkzeug.utils import secure_filename
+import io
+import os
+import h5py as h5
+from dictionary import Dictionary
+
+app = Flask(__name__, static_url_path='/static')
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def sendFile():
+    result = []
+    label = []
+    link = []
+
+    if request.method == 'POST':
+        file = request.files['img_file']
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_path_name = "/static/tmp/" + filename
+            dictionary = Dictionary(file)
+            res = dictionary.judgePhoto()
+
+            for i, acc in enumerate(res):
+                if int(acc * 100) > 0:
+                    val = int(acc * 100)
+                    src = dictionary.getLabel(i)
+                    dst = src.replace('U+', '\\u')
+
+                    uc01 = dst.encode('utf-8')
+                    uc02 = uc01.decode('utf-8')
+                    uc03 = uc02.encode().decode('unicode-escape')
+
+                    label.append(uc03)
+                    result.append(val)
+                    link.append(src)
+            return render_template("result.html", result = result, label = label, link = link, image_path=save_path_name)
+    else:
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/show_image.js
@@ -0,0 +1,20 @@
+$(function() {
+    var file = $('input[type=file]').prop('files')[0];
+
+    if(file == null) {
+        $(".btn").prop("disabled", true);
+    }
+
+    $('input[type=file]').change(function() {
+        var file = $(this).prop('files')[0];
+
+        $(".btn").prop("disabled", false);
+
+        $('#result img').remove();
+        var fileReader = new FileReader();
+        fileReader.onloadend = function() {
+            $(".img_prev").html('<img src="' + fileReader.result + '"/>');
+        }
+        fileReader.readAsDataURL(file);
+    });
+});
